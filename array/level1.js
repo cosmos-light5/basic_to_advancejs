fcf51661675b15daa86f08df2e779950818dae37
Cosmos_light5 <thapa48prakash@gmail.com>
2026-03-17 06:10:54 +0000

    level_1 practice

---
+++ array/level1.js
@@ -0,0 +1,72 @@
+// Array 
+// ---> [4,5,'3', null, undefined, true, false]]
+// based on index
+// [ ] big bracket, separated by comma
+
+let ramScore = [95, 92, 87]
+let shyamScore = [77,99,99]
+
+// Q1. calcuate the length of array
+const arraylength1=ramScore.length
+const arraylength2=shyamScore.length
+console.log(arraylength1)
+console.log(arraylength2)
+
+console.log(ramScore.length)
+console.log(shyamScore.length)
+
+
+let lastnumshyam=shyamScore[arraylength2-1]
+let divisiable2=lastnumshyam%2===0
+console.log(divisiable2)
+
+let lastElement = ramScore[ramScore.length - 1];
+console.log("Last element:", lastElement);
+
+// console.log(ramScore[ramScore.length - 1])
+
+if (lastElement % 21 === 0) {
+    console.log("divisible");
+} else {
+    console.log("NOT divisible");
+}
+
+
+
+// Q3. check if ram percentage is greater than shyam percentage
+let ramtotal=ramScore[0]+ramScore[1]+ramScore[2]
+let rampercentage=(ramtotal/300)*100
+
+let shyamtotal=shyamScore[0]+shyamScore[1]+shyamScore[2]
+let shyampercentage=(shyamtotal/300)*100
+
+let result=rampercentage>shyampercentage
+console.log(result)
+
+
+
+
+
+
+
+
+//mapvs foreach vs filter in array
+
+const a = (ramScore[0]+ramScore[1]+ramScore[2])/300 * 100;    //ramScore[initial index]+ramScore[next index]+..
+const b = (shyamScore[0]+shyamScore[1]+shyamScore[2])/300 * 100;
+
+console.log(a > b)
+
+/*// Function to calculate the average (percentage) of an array of scores
+function calculateAverage(scores) {
+    // Use the reduce method to sum all the scores in the array
+    const sum = scores.reduce((accumulator, currentScore) => accumulator + currentScore, 0);
+    // Divide the sum by the number of scores to get the average
+    const average = sum / scores.length;
+    return average;
+}
+
+// Calculate the average percentage for both Ram and Shyam
+let ramPercentage = calculateAverage(ramScore);
+let shyamPercentage = calculateAverage(shyamScore);
+ */
