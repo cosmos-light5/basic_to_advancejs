8e5a2cd9735cfdb8e96a4c002ff95559f3d8926a
Cosmos_light5 <thapa48prakash@gmail.com>
2026-03-20 13:11:42 +0000

    level_5 question1 solve

--- array/level5.js
+++ array/level5.js
@@ -7,3 +7,27 @@ const arr = [5,1,4]
 // })
 //console.log(mappedArr); // [10, 2, 8]
 //--------------------
+
+
+//Q1. if item is divisible by 2, then multiply it by 3 else multiply it by 2
+// expected output: [10, 2, 12]
+// HINT: 
+const output = arr.map((item) => {
+    if(item % 2 === 0){
+        return item * 3
+    }else{
+        return item * 2
+    }
+})
+console.log(output); 
+
+const output02 = arr.map((item) =>{
+    if (item%2 === 0){
+        return item * 3
+    }
+    else{
+        return item * 2
+    }
+})
+
+console.log(output02)
