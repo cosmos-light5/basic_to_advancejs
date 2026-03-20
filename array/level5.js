ca647cd166ca605fe085f2fb99139c2aacb93214
Cosmos_light5 <thapa48prakash@gmail.com>
2026-03-20 13:06:46 +0000

    level_5 sample

---
+++ array/level5.js
@@ -0,0 +1,9 @@
+const arr = [5,1,4]
+
+// syntax for map 
+
+// const mappedArr = arr.map((item) => {
+//     return element * 2
+// })
+//console.log(mappedArr); // [10, 2, 8]
+//--------------------
