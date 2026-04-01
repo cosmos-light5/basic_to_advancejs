c5ab25b27f0dc2c2c8bed86d24e13c07719cbf21
Cosmos_light5 <thapa48prakash@gmail.com>
2026-04-01 06:56:57 +0000

    new questions added

--- boolean/level2.js
+++ boolean/level2.js
@@ -10,3 +10,23 @@ console.log(a*5<b*2)
 
 //Check if b divided by a is greater than or equal to 2.
 console.log(b/a>=2)
+
+//Check if a is an even number AND b is an even number.
+
+
+//Check if a is less than b OR a * 3 is greater than b.
+
+
+//Check if the difference between b and a is equal to a.
+
+
+//Check if a + b is divisible by 3.
+
+
+//Check if a squared is greater than b.
+
+
+//Check if b is divisible by both a and 5.
+
+
+//Check if a is not divisible by 3.
