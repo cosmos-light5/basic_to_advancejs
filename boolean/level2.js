86d50c2dc5a67335d09c7f242e0fe4369a1b333b
Cosmos_light5 <thapa48prakash@gmail.com>
2026-04-01 06:49:04 +0000

    new questions

--- boolean/level2.js
+++ boolean/level2.js
@@ -4,3 +4,8 @@ let b=15
 
 //Check if a is NOT equal to b.
 console.log(a!=b)
+
+//Check if a multiplied by 5 is less than b multiplied by 2.
+
+
+//Check if b divided by a is greater than or equal to 2.
