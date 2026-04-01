dff5f25533958c60df19ca7a917375847beb4c3e
Cosmos_light5 <thapa48prakash@gmail.com>
2026-04-01 06:52:54 +0000

    new questions soln

--- boolean/level2.js
+++ boolean/level2.js
@@ -6,6 +6,7 @@ let b=15
 console.log(a!=b)
 
 //Check if a multiplied by 5 is less than b multiplied by 2.
-
+console.log(a*5<b*2)
 
 //Check if b divided by a is greater than or equal to 2.
+console.log(b/a>=2)
